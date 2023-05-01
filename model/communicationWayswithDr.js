03bedbf52267dff1e45b35fe8a6d7917ba4295ab
AhmedAymaan <78112105+AhmedAymaan@users.noreply.github.com>
2023-05-01 19:06:33 +0300

    Add files via upload

---
+++ model/communicationWayswithDr.js
@@ -0,0 +1,23 @@
+const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const communicationWays = new Schema({
+WhatsApp_Link: {
+  type: String,
+  },
+Telegram_Link: {
+  },
+  FaceBook_Link: {
+  type: String,
+  },
+  Gmail_Link: {
+  type: String,
+  },
+LastUpdateDate: {
+    type: Date,
+    default: Date.now()
+    }
+  }); 
+module.exports = mongoose.model('CommunicationWays', communicationWays);
