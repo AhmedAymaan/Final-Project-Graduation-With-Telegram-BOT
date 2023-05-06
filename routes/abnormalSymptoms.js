11e6550ba4fdf5e3f081820acda54685109adf15
AhmedAymaan <78112105+AhmedAymaan@users.noreply.github.com>
2023-05-06 18:34:28 +0300

    Update abnormalSymptoms.js

--- routes/abnormalSymptoms.js
+++ routes/abnormalSymptoms.js
@@ -7,6 +7,6 @@ router.get('/show-Abnormal-Symptoms-To-Doctor', abnormalSymptoms.index)
 router.post('/show-Abnormal-Symptoms-To-Doctor-By-Patient-Name', abnormalSymptoms.show)
 router.post('/store-Abnormal-Symptoms-By-Patient', abnormalSymptoms.store)
 router.put('/update-Abnormal-Symptoms-By-Patient', abnormalSymptoms.updateInfo)
-router.delete('/delete-Abnormal-Symptoms-By-Doctor-By-Patient-Name', abnormalSymptoms.destroy)
+router.delete('/delete-Abnormal-Symptoms-By-Doctor-By-Patient-ID', abnormalSymptoms.destroy)
 
 module.exports = router
