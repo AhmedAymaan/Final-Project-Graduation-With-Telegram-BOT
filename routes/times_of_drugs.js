fea83c8783567af90ba2fa104efcdc8f403da794
AhmedAymaan <78112105+AhmedAymaan@users.noreply.github.com>
2023-05-11 15:02:52 +0300

    Update times_of_drugs.js

--- routes/times_of_drugs.js
+++ routes/times_of_drugs.js
@@ -4,10 +4,12 @@ const express = require('express');
 const router = express.Router();
 
 
-router.get('/all-drugs', times_of_drugs_Controller.index)
+router.get('/all-drugs-for-patient', times_of_drugs_Controller.index)
+router.post('/all-drugs-for-doctor', times_of_drugs_Controller.show_To_Doctor)
 router.post('/drug-by-name', times_of_drugs_Controller.show)
 router.post('/store-drug', times_of_drugs_Controller.store)
-router.post('/update-drug', times_of_drugs_Controller.update)
-router.post('/delete-drug', times_of_drugs_Controller.destroy)
+router.post('/update-drug-by-doctor', times_of_drugs_Controller.update)
+router.post('/update_times_by_patient', times_of_drugs_Controller.update_times_by_patient)
+router.delete('/delete-drug', times_of_drugs_Controller.destroy)
 
 module.exports = router
