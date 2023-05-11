0ab7001be8ffa106d425e1a920fb53240aa8d908
AhmedAymaan <78112105+AhmedAymaan@users.noreply.github.com>
2023-05-11 16:59:41 +0300

    Update abnormalSymptoms.js

--- routes/abnormalSymptoms.js
+++ routes/abnormalSymptoms.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const Appointments = require('../config/appiontments')
+const abnormalSymptoms = require('../config/abnormalSymptoms')
 
-router.get('/all-appointments-for-doctor', Appointments.index)
-router.post('/apponintment-by-patientname-for-doctor', Appointments.show)
-router.post('/add-appointment-by-any-one', Appointments.store)
-router.post('/delete-all-appointments-by-doctor-id', Appointments.destroy)
+
+router.get('/show-Abnormal-Symptoms-To-Doctor', abnormalSymptoms.index)
+router.post('/show-Abnormal-Symptoms-To-Doctor-By-Patient-Name', abnormalSymptoms.show)
+router.post('/store-Abnormal-Symptoms-By-Patient', abnormalSymptoms.store)
+router.put('/update-Abnormal-Symptoms-By-Patient', abnormalSymptoms.updateInfo)
+router.delete('/delete-Abnormal-Symptoms-By-Doctor-By-Patient-ID', abnormalSymptoms.destroy)
 
 module.exports = router
