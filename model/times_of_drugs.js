8f2d791608ef0119fa9820e5fd7a97a977a86002
AhmedAymaan <78112105+AhmedAymaan@users.noreply.github.com>
2023-05-17 15:39:38 +0300

    Update times_of_drugs.js

--- model/times_of_drugs.js
+++ model/times_of_drugs.js
@@ -3,39 +3,37 @@ const Schema = mongoose.Schema;
 
 
 const times_of_Drugs = new Schema({
-    doctorID:{
-     type : String,
-     required : "Doctor ID Must Be Entered"
+    d_id: {
+        type: String
     },
-    patientID:{
-        type : String,
-        required : "Patient ID Must Be Entered"
+    patientID: {
+        type: String,
     },
-    drug_name : {
-        type : [Object],
-        required : "Name of Drug is Required."
+    drug_name: {
+        type: [Object],
+        required: "Name of Drug is Required."
     },
-    how_often : {
-        type : [Object],
-        required : "The Number Of Times The Drug is Taken is Required."
+    how_often: {
+        type: [Object],
+        required: "The Number Of Times The Drug is Taken is Required."
     },
-    time_one : {
-        type : [Object],
-        required : "Time For Taking Drug is Required."
+    time_one: {
+        type: [Object],
+        required: "Time For Taking Drug is Required."
     },
-    time_two : {
-        type : [Object]
+    time_two: {
+        type: [Object]
     },
-    time_three : {
-        type : [Object]
+    time_three: {
+        type: [Object]
     },
-    every_When :{
-        type :[Object]
+    every_When: {
+        type: [Object]
     },
-    usage_of_drug : {
-        type : [Object]
+    usage_of_drug: {
+        type: [Object]
     }
-}) 
+})
 
 const times_of_drugs = mongoose.model('times_of_drugs ', times_of_Drugs);
 module.exports = times_of_drugs;
