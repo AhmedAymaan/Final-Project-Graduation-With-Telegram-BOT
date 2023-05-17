ff69f6185d875bed9cbeb3d3678283588ca38db3
AhmedAymaan <78112105+AhmedAymaan@users.noreply.github.com>
2023-05-17 15:37:33 +0300

    Update allDoctors.js

--- routes/allDoctors.js
+++ routes/allDoctors.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { check, validationResult} = require("express-validator/check");
+const { check, validationResult } = require("express-validator/check");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const router = express.Router();
@@ -7,36 +7,65 @@ const User = require("../model/User");
 const dbconfig = require('../config/db')
 
 router.get('/show-all-doctors',
-(req, res, next) => {
+    (req, res, next) => {
 
-    User.find()
-    .then(response => {
-    res.json({
-    response
-    })  
-})
-    .catch(error => {
-        res.json({
-            message: 'An error Occured!'
-        })
-    })
-}
+        User.find()
+            .then(response => {
+                res.json({
+                    response
+                })
+            })
+            .catch(error => {
+                res.json({
+                    message: 'An error Occured!'
+                })
+            })
+    }
 ),
 
 
-router.post('/show-doctor-by-id',
-(req, res, next) => {
-    
-let doctorID = req.body.doctorID
-User.findById(doctorID)
-.then(response => {
-res.json({
-response
-})
-})
-.catch(err => {
-message : 'An error occurred'
-})
-}
-);
+    router.post('/show-doctor-by-Name',
+        (req, res, next) => {
+
+            let doctorName = req.body.doctorname
+            User.find({name : doctorName})
+                .then(response => {
+                    res.json({
+                        response
+                    })
+                })
+                .catch(err => {
+                    message: 'An error occurred'
+                })
+        }
+    ),
+    router.post('/show-doctor-by-city',
+        (req, res, next) => {
+
+            let doctorCity = req.body.doctorCity
+            User.find({city : doctorCity})
+                .then(response => {
+                    res.json({
+                        response
+                    })
+                })
+                .catch(err => {
+                    message: 'An error occurred'
+                })
+        }
+    ), router.post('/show-doctor-by-specializaion',
+        (req, res, next) => {
+
+            let doctorSpecializaion = req.body.doctorSpecializaion
+            User.find({ specialization : doctorSpecializaion})
+                .then(response => {
+                    res.json({
+                        response
+                    })
+                })
+                .catch(err => {
+                    message: 'An error occurred'
+                })
+        }
+    );
 module.exports = router;
