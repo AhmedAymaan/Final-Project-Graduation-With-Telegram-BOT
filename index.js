8c47405d26b72d10463cb9f55555d236b2bdcd2d
AhmedAymaan <78112105+AhmedAymaan@users.noreply.github.com>
2023-05-21 15:38:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -107,12 +107,12 @@ app.post(URI, async (req, res) => {
 })*/
 
 //Active Telegram CHATBOT:
-const { Telegraf } = require('telegraf');
+/*const { Telegraf } = require('telegraf');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 
-/*
+
 bot.command('ethereum', ctx => {
   var rate;
   console.log(ctx.from)
@@ -124,7 +124,7 @@ bot.command('ethereum', ctx => {
     bot.telegram.sendMessage(ctx.chat.id, message, {
     })
   })
-})*/
+})
 bot.command('start', ctx => {
   console.log(ctx.from)
   bot.telegram.sendMessage(ctx.chat.id, `
@@ -395,5 +395,64 @@ With my wishes for a speedy recovery, Ahmed Ayman Al-Saee..🤍
   })
 })
 app.use(bot.webhookCallback('/secret-path'))
-bot.telegram.setWebhook('https://eb96-62-114-104-174.eu.ngrok.io/secret-path')
+bot.telegram.setWebhook('https://eb96-62-114-104-174.eu.ngrok.io/secret-path')*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const TelegramBot = require("node-telegram-bot-api");
+const { Configuration, OpenAIApi } = require("openai");
+
+//require("dotenv").config();
+
+const configuration = new Configuration({
+    apiKey: "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",
+});
+const openai = new OpenAIApi(configuration)
+
+// replace the value below with the Telegram token you receive from @BotFather
+const token = "6092800778:AAHQOuA7PlHhsRwXT8FwDqdmos4O7WZ8k9E"
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on("message", async (msg) => {
+    const chatId = msg.chat.id;
+    const userInput = msg.text;
+
+    const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: userInput,
+        temperature: 0,
+        max_tokens: 3000,
+        top_p: 1,
+        frequency_penalty: 0.5,
+        presence_penalty: 0,
+    });
+    const generatedText = response.data.choices[0].text;
+
+    bot.sendMessage(chatId, generatedText);
+});
 
