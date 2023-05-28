ab04a3cf7bd8a551e6c42d8c30da88dd890eb372
AhmedAymaan <78112105+AhmedAymaan@users.noreply.github.com>
2023-05-28 22:48:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 const axios = require('axios')
 //Routes
 const doctorUser = require("./routes/user"); //new addition
-const dynpgfrdrRoute = require("./routes/dynpgfrdr"); 
+const dynpgfrdrRoute = require("./routes/dynpgfrdr");
 const changepassword = require("./routes/changepassword");
 const patientUser = require("./routes/patientLogIN");
 const allDoctors = require("./routes/allDoctors");
@@ -107,9 +107,10 @@ app.post(URI, async (req, res) => {
 })*/
 
 //Active Telegram CHATBOT:
-/*const { Telegraf } = require('telegraf');
 
-const bot = new Telegraf(process.env.BOT_TOKEN);
+
+/*const bot = new Telegraf(process.env.BOT_TOKEN);
+
 
 
 
@@ -393,9 +394,7 @@ We hope that the personal assistant was able to help you answer common questions
 With my wishes for a speedy recovery, Ahmed Ayman Al-Saee..🤍
   `, {
   })
-})
-app.use(bot.webhookCallback('/secret-path'))
-bot.telegram.setWebhook('https://eb96-62-114-104-174.eu.ngrok.io/secret-path')*/
+})*/
 
 
 
@@ -406,53 +405,3 @@ bot.telegram.setWebhook('https://eb96-62-114-104-174.eu.ngrok.io/secret-path')*/
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-
-const TelegramBot = require("node-telegram-bot-api");
-const { Configuration, OpenAIApi } = require("openai");
-
-//require("dotenv").config();
-
-const configuration = new Configuration({
-    apiKey: "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",
-});
-const openai = new OpenAIApi(configuration)
-
-// replace the value below with the Telegram token you receive from @BotFather
-const token = "6092800778:AAHQOuA7PlHhsRwXT8FwDqdmos4O7WZ8k9E"
-
-const bot = new TelegramBot(token, { polling: true });
-
-bot.on("message", async (msg) => {
-    const chatId = msg.chat.id;
-    const userInput = msg.text;
-
-    const response = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: userInput,
-        temperature: 0,
-        max_tokens: 3000,
-        top_p: 1,
-        frequency_penalty: 0.5,
-        presence_penalty: 0,
-    });
-    const generatedText = response.data.choices[0].text;
-
-    bot.sendMessage(chatId, generatedText);
-});
-*/
